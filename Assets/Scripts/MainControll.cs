d1997c8feb6a83d2042634ae4f4590578609c1f1
TingLans <lr030322@163.com>
2023-06-05 17:22:29 +0800

    固定帧率

---
+++ Assets/Scripts/MainControll.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainControll : MonoBehaviour
+{
+    private void Awake()
+    {
+        Application.targetFrameRate = 60; // ÓÎÏ·Ö¡ÂÊ
+    }
+}
