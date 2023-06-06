8baa29a2134e4a4dc4b0f1b43b8b64e653e35ec5
wwwangzd <2436579671@qq.com>
2023-06-06 22:09:47 +0800

    1

--- Assets/Scripts/Level2/AttackUI.cs
+++ Assets/Scripts/Level2/AttackUI.cs
@@ -9,54 +9,55 @@ public class AttackUI : MonoBehaviour
     GameObject bullet;
     GameObject enemy;
 
-    int  flag = 0;
+    int flag = 0;
     // Start is called before the first frame update
     void Start()
     {
         enemy = GameObject.Find("virus");
-        if(enemy)    StartCoroutine("shoot");
+        if (enemy) StartCoroutine("shoot");
     }
 
     // Update is called once per frame
     void Update()
     {
+
         //开始和停止射击机制
-        if(enemy == null)
+        if (enemy == null)
         {
-            if(flag == 0) StopCoroutine("shoot");
+            if (flag == 0) StopCoroutine("shoot");
             enemy = GameObject.Find("virus");
-            if(enemy == null) flag = -1;
+            if (enemy == null) flag = -1;
             else flag = 1;
         }
-        else if(flag == 1)
+        else if (flag == 1)
         {
             // StartCoroutine("shoot");
             // flag = 0;
-            if(BulletControll.mindis <= 12.0f)
-            { 
+            if (BulletControll.mindis <= 12.0f)
+            {
                 StartCoroutine("shoot");
                 flag = 0;
             }
         }
+
         //不能，因为还要进去才能判断距离，否则midis不更新，死锁
         // else if(flag == 0 && BulletControll.mindis > 12.0f)
         // {
         //     StopCoroutine("shoot");
         //     flag = 1;
         // }
+        //生成子弹
     }
-
-    //生成子弹
     IEnumerator shoot()
     {
-        
+
         while (true)
         {
             yield return new WaitForSeconds(1.0f);
             bullet = Instantiate(bulletprefab, launchplace.transform.position, bulletprefab.transform.rotation, this.transform);
             bullet.name = "bullet";
-        //    Debug.Log(bullet.transform.position);
-        //  Destroy(bullet,5.0f);        //在子弹预制体里面实现
+            //    Debug.Log(bullet.transform.position);
+            //  Destroy(bullet,5.0f);        //在子弹预制体里面实现
         }
 
     }
